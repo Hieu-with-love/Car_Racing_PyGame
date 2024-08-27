03c65b97ef2966992cf246a0c0c1a8b8d30271d2
hieuzeus <hieu01bdvn@gmail.com>
2024-08-27 16:03:15 +0700

    init racing car game

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+import time
+import math
+
+# constant with convention is capital all character
+GRASS = pygame.image.load("imgs/grass.jpg")
+TRACK = pygame.image.load("imgs/track.png")
+
+TRACK_BODER = pygame.image.load("imgs/track-border.png")
+FINISH = pygame.image.load("imgs/finish.png")
+
+RED_CAR = pygame.image.load("imgs/red-car.png")
+GREEN_CAR = pygame.image.load("imgs/green-car.png")
+
+
+
